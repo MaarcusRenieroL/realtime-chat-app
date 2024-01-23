a66be9288b3536251d404e417312f8922b1f7012
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-01-23 11:30:07 +0530

    feat: add backend functionality for next auth

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error("Missing Google Client Id");
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error("Missing Google Client Secret");
+  }
+
+  return { clientId, clientSecret };
+}
